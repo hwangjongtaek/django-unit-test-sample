1af3860edac16ee4d7b7952eb8730f08b8418e5a
hwangjongtaek <hwang.jongtaek@gmail.com>
2019-04-04 07:27:51 +0900

    Sample Unit Test added

--- crawler/tests.py
+++ crawler/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class CrawlerTests(TestCase):
+
+    def test_1(self):
+        self.assertEqual(1, 1)
+
+    def test_2(self):
+        self.assertEqual(3, 3)
+
+    def test_3(self):
+        self.assertEqual(4, 4)
+
+
+class ConverterTests(TestCase):
+
+    def test_1(self):
+        self.assertEqual(1, 1)
+
+    def test_2(self):
+        self.assertEqual(3, 3)
+
+    def test_3(self):
+        self.assertEqual(4, 4)
