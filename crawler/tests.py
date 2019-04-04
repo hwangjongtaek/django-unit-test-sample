bd1f38bd5537de7a449e773e94f9e625728fe981
hwangjongtaek <hwang.jongtaek@gmail.com>
2019-04-05 08:45:06 +0900

    crawler added, unittest added

--- crawler/tests.py
+++ crawler/tests.py
@@ -1,25 +1,38 @@
 from django.test import TestCase
+from crawler import views
 
 
 class CrawlerTests(TestCase):
 
-    def test_1(self):
-        self.assertEqual(1, 1)
+    test_url = 'http://kpat.kipris.or.kr/kpat/biblioa.do' \
+               '?method=biblioMain_biblio' \
+               '&next=biblioViewSub01' \
+               '&applno=2020000012789' \
+               '&getType=BASE'
 
-    def test_2(self):
-        self.assertEqual(3, 3)
+    crawler = views.Crawler()
+    normalizer = views.Normalizer()
 
-    def test_3(self):
-        self.assertEqual(4, 4)
+    result = crawler.get_contents_from_url(test_url)
+    body = crawler.analyze_html(result.text)
+    tag_removed_body = normalizer.tag_remover(body.get_text())
+    line_breaker_removed_body = normalizer.tag_remover(tag_removed_body)
+    nbsp_removed_body = normalizer.nbsp_remover(line_breaker_removed_body)
 
+    def test_get_contents_from_url(self):
+        self.assertIs(self.result.status_code, 200)
 
-class ConverterTests(TestCase):
+    def test_contents_encoding_is_utf8(self):
+        self.assertEqual(self.result.encoding, 'UTF-8')
 
-    def test_1(self):
-        self.assertEqual(1, 1)
+    def test_extract_body_from_html(self):
+        self.assertIsNotNone(self.body)
 
-    def test_2(self):
-        self.assertEqual(3, 3)
+    def test_extract_html_tag_from_html(self):
+        self.assertIsNotNone(self.tag_removed_body)
 
-    def test_3(self):
-        self.assertEqual(4, 4)
+    def test_extract_line_breaker_from_html(self):
+        self.assertIsNotNone(self.tag_removed_body)
+
+    def test_extract_nbsp_from_html(self):
+        self.assertIsNotNone(self.nbsp_removed_body)
--- crawler/views.py
+++ crawler/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+import re
+import requests
+from bs4 import BeautifulSoup
 
-# Create your views here.
+
+class Crawler(object):
+
+    def get_contents_from_url(self, url):
+        target_contents = requests.get(url)
+        return target_contents
+
+    def analyze_html(self, html):
+        soup = BeautifulSoup(html, 'html.parser')
+        body = soup.body
+        return body
+
+
+class Normalizer(object):
+
+    def tag_remover(self, text):
+        cleaner = re.compile('<.*?>')
+        clean_text = re.sub(cleaner, '', text)
+
+        return clean_text
+
+    def line_breaker_remover(self, text):
+        clean_text = text.replace('\n', '')
+        return clean_text
+
+    def nbsp_remover(self, text):
+        clean_text = text.replace('nbsp;', '')
+        return clean_text
