c59177db7a0f5eb956b57a8e618f7386a0b6af2d
hwangjongtaek <hwang.jongtaek@gmail.com>
2019-05-03 16:28:11 +0900

    setUp method added

--- crawler/tests.py
+++ crawler/tests.py
@@ -3,7 +3,6 @@ from crawler import views
 
 
 class CrawlerTests(TestCase):
-
     test_url = 'http://kpat.kipris.or.kr/kpat/biblioa.do' \
                '?method=biblioMain_biblio' \
                '&next=biblioViewSub01' \
@@ -19,20 +18,39 @@ class CrawlerTests(TestCase):
     line_breaker_removed_body = normalizer.tag_remover(tag_removed_body)
     nbsp_removed_body = normalizer.nbsp_remover(line_breaker_removed_body)
 
+    @classmethod
+    def setUpTestData(cls):
+        pass
+
+    def setUp(self):
+        pass
+
     def test_get_contents_from_url(self):
-        self.assertIs(self.result.status_code, 200)
+        assert_result = self.assertIs(self.result.status_code, 200)
+        print(assert_result)
 
     def test_contents_encoding_is_utf8(self):
-        self.assertEqual(self.result.encoding, 'UTF-8')
+        assert_result = self.assertEqual(self.result.encoding, 'UTF-8')
+        self.assert_error_logger(assert_result)
 
     def test_extract_body_from_html(self):
-        self.assertIsNotNone(self.body)
+        assert_result = self.assertIsNotNone(self.body)
+        self.assert_error_logger(assert_result)
 
     def test_extract_html_tag_from_html(self):
-        self.assertIsNotNone(self.tag_removed_body)
+        assert_result = self.assertIsNotNone(self.tag_removed_body)
+        self.assert_error_logger(assert_result)
 
     def test_extract_line_breaker_from_html(self):
-        self.assertIsNotNone(self.tag_removed_body)
+        assert_result = self.assertIsNotNone(self.tag_removed_body)
+        self.assert_error_logger(assert_result)
 
     def test_extract_nbsp_from_html(self):
-        self.assertIsNotNone(self.nbsp_removed_body)
+        assert_result = self.assertIsNotNone(self.nbsp_removed_body)
+        self.assert_error_logger(assert_result)
+
+    def assert_error_logger(self, result):
+        if result:
+            print(result)
+
+        return True
